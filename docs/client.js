3b470dad2501521ff05bb078d82705421cdf49eb
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2024-04-24 10:07:31 -0400

    Client check github

--- docs/client.js
+++ docs/client.js
@@ -2,10 +2,13 @@
 /**
  * First step in connecting to a multiplayer server on Khan Academy.
  *
- * @param {string} connectionString - Connection string in the format of ip:port like 123.1.1.1:4444
+ * @param {string} connectionString
+ * - Connection string in the format of ip:port:github-user/github-repo/branch
+ * - Example: 123.1.1.1:4444:kqwq/zeta6/dev
  */
 async function connect(connectionString) {
   // Step 1.1: Prepare packets for the connection offer
+  const [ip, port, repo] = connectionString.split(":");
   const uuid = Math.random().toString(36).substring(2, 15);
   const pc = new RTCPeerConnection();
   const offer = await pc.createOffer();
@@ -14,12 +17,12 @@ async function connect(connectionString) {
     if (event.candidate) {
       console.log("candidate", event.candidate);
     }
-  }
+  };
   pc.ondatachannel = (event) => {
     const chat = event.channel;
     chat.onmessage = (event) => console.log("message", event.data);
     chat.send("Hello, world! from client");
-  }
+  };
   const offerPackets = [];
   const maxPacketSize = 200; // Must fit in 256 bytes
   let i = 0;
@@ -37,7 +40,7 @@ async function connect(connectionString) {
     const disposablePC = new RTCPeerConnection({
       iceServers: [
         {
-          urls: [`turn:${connectionString}`],
+          urls: [`turn:${ip}:${port}`],
           username: packet,
           credential: "1",
         },
@@ -47,6 +50,15 @@ async function connect(connectionString) {
   }
 
   console.log("offer", offer.sdp.length, offer.sdp);
-}
 
-// Step 3 (step 2 is in the server code)
+  // Step 3 (step 2 is in the server code)
+  // Attempt to fetch from the GitHub repository through jsdelivr
+  const [ghUser, ghRepo, ghBranch] = repo.split("/");
+  const url = `https://cdn.jsdelivr.net/gh/${ghUser}/${ghRepo}@${ghBranch}/offers/${uuid}.txt`;
+  const response = await fetch(url);
+  const answer = await response.text();
+  console.log("answer1", answer.length, answer);
+  pc.setRemoteDescription({ type: "answer", sdp: answer });
+
+  
+}
